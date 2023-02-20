ab24e82303057b212474a622b5fb8ffba45bab70
Emiraola <emontero.iraola@gmail.com>
2023-02-20 14:42:08 +0100

    main -- a little bit or order to the project plus more actions

---
+++ Assets/Scripts/GOAPActions/GoToRegister.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+    public class GoToRegister : GAction
+    {
+
+
+    public override bool PrePerform()
+    {
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+      return true;
+    }
+}
